f43b7a6aff60bb2e4a7a37066d7aa077e8ce864a
Aiganym Shynarbek <aiganym.shynarbek@nu.edu.kz>
2025-05-13 15:24:06 +0500

    Order Service project

---
+++ OrderService/Controllers/OrdersController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using OrderService.Data;
+using OrderService.Models;
+
+namespace OrderService.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class OrdersController : ControllerBase
+    {
+        private readonly OrderDbContext _context;
+
+        public OrdersController(OrderDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/orders
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Order>>> GetOrders()
+        {
+            return await _context.Orders.ToListAsync();
+        }
+
+        // GET: api/orders/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Order>> GetOrder(int id)
+        {
+            var order = await _context.Orders.FindAsync(id);
+
+            if (order == null)
+                return NotFound();
+
+            return order;
+        }
+
+        // POST: api/orders
+        [HttpPost]
+        public async Task<ActionResult<Order>> PostOrder(Order order)
+        {
+            _context.Orders.Add(order);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetOrder), new { id = order.Id }, order);
+        }
+
+        // DELETE: api/orders/1
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteOrder(int id)
+        {
+            var order = await _context.Orders.FindAsync(id);
+            if (order == null)
+                return NotFound();
+
+            _context.Orders.Remove(order);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+    }
+}
---
+++ OrderService/Models/Order.cs
@@ -0,0 +1,11 @@
+﻿namespace OrderService.Models
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public string CustomerName { get; set; }
+        public string Product { get; set; }
+        public int Quantity { get; set; }
+        public DateTime OrderDate { get; set; }
+    }
+}
